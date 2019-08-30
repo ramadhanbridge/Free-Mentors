d196f0499b6635d9e0caea7a973a8012f59606f2
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-08-30 07:21:38 +0200

    ft(accept):add accept request api
    add accept api
    
    [starts #168096649]

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import PORT from './config/port';
 import authroute from './routes/Auth';
 import adminroute from './routes/admin';
 import menteeroute from './routes/mentee';
+import mentorroute from './routes/mentor';
 import not_found from './routes/default';
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 
 app.use('/api/v1', authroute);
 app.use('/api/v1', adminroute);
+app.use('/api/v1', mentorroute);
 app.use('/api/v1', menteeroute);
 
 app.use(not_found);
