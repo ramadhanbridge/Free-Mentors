a98db62014c832f730cfa8ca60ad68283f81a01f
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-09-03 02:30:03 +0200

    ch(docs):add swagger

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import PORT from './config/port';
 import authroute from './routes/Auth';
 import adminroute from './routes/admin';
 import menteeroute from './routes/mentee';
+import swaggerui from 'swagger-ui-express';
+import api_documentation from './documentation/swagger.json'
 import mentorroute from './routes/mentor';
 import sessionroute from './routes/sessions';
 import not_found from './routes/default';
@@ -17,6 +19,7 @@ app.use('/api/v1', adminroute);
 app.use('/api/v1', mentorroute);
 app.use('/api/v1', menteeroute);
 app.use('/api/v1', sessionroute);
+app.use('/documentation',swaggerui.serve,swaggerui.setup(api_documentation));
 
 
 app.use(not_found);
