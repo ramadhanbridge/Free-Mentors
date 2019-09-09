1c8272bfc33b5196833f926b15016f374f2b3790
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-09-09 22:14:30 +0200

    chore(dummy): add dummy data
    with this commit, I will be adding default users
     in database
    
    [finishes #168364828]

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,6 @@
 import  conn  from '../config/db'
+import { config } from 'dotenv';
+config();
 
 class Db_queries{
 
@@ -14,13 +16,63 @@ class Db_queries{
      let conn = this.connection;
      await conn.connect()
      await conn.query(`
+     DROP TABLE IF EXISTS users CASCADE;
+
      CREATE TABLE IF NOT EXISTS users ( id SERIAL, firstName VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, expertise TEXT ,email VARCHAR(50) UNIQUE NOT NULL, occupation TEXT, password TEXT NOT NULL, address VARCHAR(50) NOT NULL, role VARCHAR(20) NOT NULL,Bio TEXT , PRIMARY KEY (id));
 
-     CREATE TABLE IF NOT EXISTS sessions ( sessionId SERIAL, mentorId INTEGER , menteeId INTEGER,  mentor_name  VARCHAR(50) NOT NULL, questions  TEXT , menteeName VARCHAR(255) UNIQUE NOT NULL, menteeEmail VARCHAR(50) UNIQUE NOT NULL , status VARCHAR(50) NOT NULL  ,PRIMARY KEY (sessionId));
+     CREATE TABLE IF NOT EXISTS sessions ( sessionId SERIAL, mentorId INTEGER  references users(id) ON DELETE CASCADE, menteeId INTEGER references users(id) ON DELETE CASCADE,  mentor_name  VARCHAR(50) NOT NULL, questions  TEXT , menteeName VARCHAR(255) NOT NULL, menteeEmail VARCHAR(50)  , status VARCHAR(50) NOT NULL  ,PRIMARY KEY (sessionId));
    
-     CREATE TABLE IF NOT EXISTS reviews (  sessionId SERIAL, mentorId INTEGER , menteeId INTEGER, remark  TEXT NOT NULL, menteeFullname  TEXT  , score INTEGER,PRIMARY KEY (sessionId));
+     CREATE TABLE IF NOT EXISTS reviews (  sessionId SERIAL, mentorId INTEGER  references users(id) ON DELETE CASCADE , menteeId INTEGER references users(id) ON DELETE CASCADE, remark  TEXT NOT NULL, menteeFullname  TEXT  , score INTEGER,PRIMARY KEY (sessionId));
     
         `);
+    await conn.query(`INSERT INTO users(firstName,lastname,expertise,email,occupation,role,password,address,Bio) VALUES(
+          'admin',
+          'lastname',
+          'programmer',
+          '$admin@gmail.com',
+          'swimming',
+          'admin',
+          '${process.env.FAKE_PASS_WORD}',
+          'kigali',
+          'i like animals'
+        ) 
+      `);
+      await conn.query(`INSERT INTO users(firstName,lastname,expertise,email,occupation,role,password,address,Bio) VALUES(
+        'ramadhan',
+        'lastname',
+        'programmer',
+        'ramadhan@gmail.com',
+        'swimming',
+        'mentee',
+        '${process.env.FAKE_PASS_WORD}',
+        'kigali',
+        'i like animals'
+
+        ) 
+       `);
+    await conn.query(`INSERT INTO users(firstName,lastname,expertise,email,occupation,role,password,address,Bio) VALUES(
+      'ezila',
+      'lastname',
+      'programmer',
+      '$ezila@gmail.com',
+      'swimming',
+      'mentor',
+      '${process.env.FAKE_PASS_WORD}',
+      'kigali',
+      'i like animals'
+
+       ) 
+      `);
+    await conn.query(`INSERT INTO sessions(mentorId , menteeId ,  mentor_name , questions , menteeName , menteeEmail, status ) VALUES(
+        1,
+        1,
+        'ezila',
+        'why so serious',
+        'ramadhan',
+        'ramadhan@gmail.com',
+        'request'
+       ) 
+    `);
 
     await conn.end();
 
