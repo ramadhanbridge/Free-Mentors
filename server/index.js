09f7cb929d39a433b5c361f57c8956799e6acb8f
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-09-10 08:33:29 +0200

    chore(refactor): reafactor index
    I am going to refactor index by including, things that i created in db
    
    [finishes #168397150]

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import PORT from './config/port';
 import routes from './routes/routes_index'
 import not_found from './routes/default';
-
+import db_init from './models/db';
 const swagger = require('swagger-ui-express');
 const Api_documentation = require('./swagger.json');
 const app = express();
+
+db_init.create_tables();
 
 app.use('/api/v2',routes );
 app.use('/documentation', swagger.serve, swagger.setup(Api_documentation));
