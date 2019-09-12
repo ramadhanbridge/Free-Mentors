3c7548ab46b1e014032d1147c40da1151e909d90
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-09-12 13:05:56 +0200

    feature(reject): reject session
    mentor should be able to create mentorship session
    
    [finishes #168412626]

--- server/controllers/mentor_controller.js
+++ server/controllers/mentor_controller.js
@@ -11,13 +11,8 @@ accept_session= async (req,res) =>
 
 reject_session=(req,res)=>
 {
-const session_tobe_rejected=db.reject_session(req.params.sessionId);
-if(!session_tobe_rejected){return res.status(404).json({status:404,message:"session does not exist"})}
-else
-{
-    session_tobe_rejected.status="rejected"
-   return  res.status(200).json({status:200,data:{message:"request rejected",data:session_tobe_rejected}})
-}
+    let accepted = await  db.reject_session(req.params.sessionId)
+    return message.success(res,200,"request accepted",accepted)
 }
 
 
--- server/middleware/mentor_middleware.js
+++ server/middleware/mentor_middleware.js
@@ -10,16 +10,13 @@ class Mentor
     next();
     }
 
-request_Validate = async (req,res, next)=> 
-{
 
-if(req.body.mentorId === undefined ||req.body.mentor_name === undefined || req.body.question === undefined )
-{
-return message.error(res,400,"unvalid (mentorId,question,mentor_name)")
-}
-next();
-
-} 
+    reject = async (req,res, next) =>
+    {
+    const session_tobe_accepted=await db.accept_session(req.params.sessionId);
+    if(!session_tobe_accepted){return message.error(res,400,"session does not exist")}
+    next();
+    }
 
 
 
